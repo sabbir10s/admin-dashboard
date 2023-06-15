b8a046964ace20f0ca4e167edf0157dbeeab23d6
alamincoders <developeralamin03@gmail.com>
2023-06-16 02:23:03 +0600

    added tailwind in this project

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -3,21 +3,31 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
-      <header>Header</header>
+      <header>
+        {' '}
+        <h1 className="text-3xl font-bold underline bg-primary-400 text-white py-2">
+          Header!
+        </h1>
+      </header>
       <main>
-        <div className="layout">
-          <aside className="sidebar">
+        <div className="layout flex w-full">
+          <aside className="sidebar bg-primary-100 p-10 w-full">
             {/* Add sidebar content */}
             Sidebar
           </aside>
 
-          <main className="main-content">
+          <main className="main-content bg-primary-400 text-white p-10 w-full">
             {/* Add main content */}
             {children}
           </main>
         </div>
       </main>
-      <footer>Footer</footer>
+      <footer>
+        {' '}
+        <h1 className="text-3xl font-bold underline bg-primary-600 text-white py-2">
+          Hello Footer !
+        </h1>
+      </footer>
     </React.Fragment>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f3f6fc',
+          100: '#e5edf9',
+          200: '#c6daf1',
+          300: '#93bbe6',
+          400: '#5997d7',
+          500: '#347bc3',
+          600: '#2460a5',
+          700: '#1e4c86',
+          800: '#1d426f',
+          900: '#1c375a',
+          950: '#13253e',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
