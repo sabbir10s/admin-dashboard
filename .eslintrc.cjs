a9a541b20790f442c4d57a124029fb7e9d61708c
alamincoders <developeralamin03@gmail.com>
2023-06-16 00:24:15 +0600

    prettier setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,39 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/react',
+    'plugin:prettier/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    'import',
+    'prettier',
+  ],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off', // Disable prop-types rule
+    'react/jsx-indent': ['error', 2], // Customize JSX indentation
   },
-}
+};
