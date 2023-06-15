be9152a2b4867d4db9d5f4d19cb647bc0bca1369
alamincoders <developeralamin03@gmail.com>
2023-06-16 00:42:23 +0600

    eslint and prettier setup done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:import/errors',
@@ -16,13 +15,17 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2022,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: {
+      version: '18.2', // Specify the version of React you are using
+    },
+  },
   plugins: [
     'react-refresh',
     'react',
@@ -32,8 +35,63 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-    'react/prop-types': 'off', // Disable prop-types rule
-    'react/jsx-indent': ['error', 2], // Customize JSX indentation
+    // General ESLint Rules
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'no-alert': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-template': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-destructuring': 'error',
+
+    // React-specific Rules
+    'react/prop-types': 'off', // Disable prop-types since React now has TypeScript/PropTypes
+    'react/jsx-boolean-value': ['error', 'always'],
+    'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
+    'react/jsx-curly-spacing': ['error', 'never'],
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-key': 'error',
+    'react/jsx-max-props-per-line': [
+      'error',
+      { maximum: 1, when: 'multiline' },
+    ],
+    'react/jsx-no-bind': ['error', { allowArrowFunctions: true }],
+    'react/jsx-no-useless-fragment': 'error',
+    'react/jsx-pascal-case': 'error',
+    'react/jsx-props-no-spreading': 'off', // Enable this rule if you don't use prop spreading
+    'react/react-in-jsx-scope': 'off', // Enable this rule if you're not using React 17+ with automatic JSX transformation
+
+    // React Hooks Rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // Import Rules
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+
+    // Prettier Rules
+    'prettier/prettier': 'error',
   },
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,25 @@
-import { useState } from 'react';
-
 function App() {
   return (
-    <>
-      <header>
-        <section>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-          blanditiis dolor ea magnam minus veritatis voluptatem facilis eligendi
-          sit, dicta odio delectus itaque repellendus officiis aspernatur sint
-          alias corporis, obcaecati quo, deserunt hic tenetur eum. Eius officia,
-          sunt nisi, similique minima libero excepturi nostrum dolorem veniam
-          repellat eaque nihil. Totam nisi, ratione qui illum voluptatibus ex
-          consectetur, blanditiis reprehenderit placeat quaerat debitis incidunt
-          dignissimos. Perferendis unde dolores recusandae harum quaerat.
-          Doloremque quos veniam fugit reiciendis distinctio! Voluptate magni
-          placeat dolore quis omnis animi expedita quasi porro sunt voluptas,
-          officiis voluptatibus ipsam explicabo commodi tempora mollitia ipsa,
-          nihil possimus asperiores. Illum magni natus deleniti accusamus
-          eligendi dicta iure alias cupiditate voluptatem. Dolores nam odio
-          quod, iste perspiciatis ab nulla! Fugit doloremque laboriosam sunt,
-          vero quos consequuntur error odit id officiis dolore magni aperiam
-          officia. Nobis expedita laboriosam unde. A, minima placeat ducimus
-          cumque aut ab dolores maxime deleniti veniam architecto doloremque
-          itaque vel totam sed nulla quisquam. Omnis reprehenderit ea inventore,
-          est iste quod dolores impedit provident, ipsa commodi expedita
-          deserunt, distinctio optio in quidem dolorum voluptates natus
-          molestias neque quae dolor perferendis odit vitae? Rem temporibus
-          maxime, expedita totam vero ut non a fuga eligendi inventore
-          praesentium dicta, beatae necessitatibus perferendis quae repellat
-          pariatur voluptatem facere ullam porro amet alias voluptatibus sed.
-          Qui quisquam, non, nam, temporibus voluptas itaque odit consequuntur
-          laborum dignissimos voluptatibus eos perferendis! Maxime nostrum unde
-          alias quisquam, iste consectetur voluptates quasi veritatis quidem eos
-          eaque totam ea exercitationem illo corrupti laboriosam, autem officia.
-          Distinctio incidunt at esse tempora amet! Praesentium commodi, impedit
-          sapiente laboriosam iure voluptates molestias ad, nemo necessitatibus
-          magnam corrupti doloribus dolores cumque accusantium suscipit, nam eum
-          saepe similique mollitia accusamus perferendis voluptatibus deleniti
-          quod. Alias suscipit id, neque architecto facilis praesentium nostrum
-          deleniti eaque perspiciatis ullam tempore dolor cumque nisi minus
-          omnis adipisci fugiat labore commodi odio assumenda error laudantium!
-          Odio dignissimos eius ducimus ad iste nesciunt? Corrupti ab
-          consectetur officiis ea inventore totam cumque minus maxime voluptates
-          libero corporis ut consequatur, sed provident facere unde veniam
-          debitis adipisci id fugit laudantium nulla earum sequi labore. Eius
-          beatae facere nobis nam iste, mollitia optio consequuntur. Eum libero
-          nam amet possimus earum asperiores expedita aliquam placeat recusandae
-          in, ad nesciunt dolorum repellendus tempore! Tempore, doloribus
-          sapiente officiis cumque consequuntur ad magnam autem, quaerat labore
-          eius earum adipisci voluptate ipsum deserunt dolore deleniti?
-          Consectetur nam in adipisci facere odio doloribus laudantium cum et
-          voluptates. Quaerat tempora rerum cum voluptatibus voluptatem illum
-          eum inventore quis ullam. Distinctio dicta sed assumenda cum deleniti
-          commodi possimus molestiae. Reprehenderit dolor totam recusandae a
-          neque iusto cumque debitis vero omnis id quam sapiente reiciendis
-          culpa nulla ea sunt porro illum minima quos molestiae quo
-          consequuntur, odio rerum error! Accusamus voluptatibus, ea voluptas
-          quaerat officia exercitationem, tempore nostrum, explicabo fugiat
-          deserunt expedita doloremque delectus ad debitis sed eos similique.
-          Quas earum non minus commodi in reiciendis. Necessitatibus dolor vero
-          nemo hic tenetur ea porro veritatis nesciunt explicabo. Quos, numquam
-          necessitatibus dolorem voluptates similique, nesciunt eligendi dolores
-          delectus tempora, omnis alias harum?
-        </section>
-      </header>
-    </>
+    <header>
+      <section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+        blanditiis dolor ea magnam minus veritatis voluptatem facilis eligendi
+        sit, dicta odio delectus itaque repellendus officiis aspernatur sint
+        alias corporis, obcaecati quo, deserunt hic tenetur eum. Eius officia,
+        sunt nisi, similique minima libero excepturi nostrum dolorem veniam
+        repellat eaque nihil. Totam nisi, ratione qui illum voluptatibus ex
+        consectetur, blanditiis reprehenderit placeat quaerat debitis incidunt
+        dignissimos. Perferendis unde dolores recusandae harum quaerat.
+        Doloremque quos veniam fugit reiciendis distinctio! Voluptate magni
+        placeat dolore quis omnis animi expedita quasi porro sunt voluptas,
+        officiis voluptatibus ipsam explicabo commodi tempora mollitia ipsa,
+        nihil possimus asperiores. Illum magni natus deleniti accusamus eligendi
+        dicta iure alias cupiditate voluptatem. Dolores nam odio quod, iste
+        perspiciatis ab nulla! Fugit doloremque laboriosam sunt, vero quos
+        consequuntur error odit id officiis dolore magni aperiam officia. Nobis
+        expedita laboriosam unde.
+      </section>
+    </header>
   );
 }
 
