552a08426d7d3dc9d6f8a60127d12a37e0dffb96
sabbir10s <sabbirahmed1023@gmail.com>
2023-06-20 21:46:47 +0600

    group button added

--- src/pages/Elements/Elements.jsx
+++ src/pages/Elements/Elements.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Avatar from '../../components/ui/Elements/Avatar/Avatar';
+import ButtonGroup from '../../components/ui/Elements/ButtonGroup/ButtonGroup';
 import DropDowns from '../../components/ui/Elements/Dropdowns/DropDowns';
 
 const Elements = () => {
   return (
     <div className=" space-y-8">
+      <ButtonGroup />
       <DropDowns />
       <Avatar />
     </div>
