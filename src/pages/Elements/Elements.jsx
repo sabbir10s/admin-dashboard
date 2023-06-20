d932940b3e95a6516e1514905c3b1e78f01d9c32
sabbir10s <sabbirahmed1023@gmail.com>
2023-06-20 22:18:43 +0600

    badges added

--- src/pages/Elements/Elements.jsx
+++ src/pages/Elements/Elements.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Avatar from '../../components/ui/Elements/Avatar/Avatar';
+import Badges from '../../components/ui/Elements/Badges/Badges';
 import ButtonGroup from '../../components/ui/Elements/ButtonGroup/ButtonGroup';
 import DropDowns from '../../components/ui/Elements/Dropdowns/DropDowns';
 
 const Elements = () => {
   return (
     <div className=" space-y-8">
+      <Badges />
       <ButtonGroup />
       <DropDowns />
       <Avatar />
