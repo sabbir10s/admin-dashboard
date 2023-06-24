b47bda4372b2afbe0ecca454faea56569f346471
sabbir10s <sabbirahmed1023@gmail.com>
2023-06-24 22:34:56 +0600

    off canvas and on canvas sidebar added

--- src/components/common/Sidebar/SidebarBodyScrolling.jsx
+++ src/components/common/Sidebar/SidebarBodyScrolling.jsx
@@ -1,35 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const SidebarOffCanvas = ({ children, isOpen, onClose }) => {
+const SidebarBodyScrolling = ({ children, isOpenBodyScrolling, onClose }) => {
   const handleCloseModal = (e) => {
     if (e.target.id === 'sidebarContainer') onClose();
   };
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, [isOpen]);
-
-  if (!isOpen) {
+  if (!isOpenBodyScrolling) {
     return null;
   }
 
   return (
     <button className="cursor-default" onClick={handleCloseModal}>
       <div>
-        {isOpen && (
+        {isOpenBodyScrolling && (
           <div className="fixed inset-0 z-50 ">
-            <div
-              id="sidebarContainer"
-              className="absolute inset-0 bg-black opacity-50"
-            ></div>
+            <div id="sidebarContainer" className="absolute inset-0"></div>
             <div className="bg-white dark:bg-gray-800 p-4 max-w-md z-10 h-screen w-64 relative">
               <button
                 className="absolute right-0 top-0 text-gray-600 dark:text-gray-300 p-4 z-50"
@@ -59,4 +44,4 @@ const SidebarOffCanvas = ({ children, isOpen, onClose }) => {
   );
 };
 
-export default SidebarOffCanvas;
+export default SidebarBodyScrolling;
