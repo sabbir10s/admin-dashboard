edfe39935b1133f47f1d8512e1361ad4f87f22ae
sabbir10s <sabbirahmed1023@gmail.com>
2023-06-24 20:39:16 +0600

    sidbar added and buttons are updated

--- src/components/common/Sidebar/SidebarOffCanvas.jsx
+++ src/components/common/Sidebar/SidebarOffCanvas.jsx
@@ -1,36 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const SidebarOffCanvas = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleModal = () => {
-    setIsOpen(!isOpen);
+const SidebarOffCanvas = ({ children, isOpen, onClose }) => {
+  const handleCloseModal = (e) => {
+    if (e.target.id === 'sidebarContainer') onClose();
   };
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <button
-        className="bg-blue-500 text-white font-semibold py-2 px-4 rounded focus:outline-none"
-        onClick={toggleModal}
-      >
-        Open Modal
-      </button>
 
-      {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="bg-white rounded-lg p-4 max-w-md mx-auto z-10">
-            <h2 className="text-lg font-semibold mb-4">Offcanvas Modal</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <button
-              className="mt-4 bg-blue-500 text-white font-semibold py-2 px-4 rounded focus:outline-none"
-              onClick={toggleModal}
-            >
-              Close
-            </button>
+  if (!isOpen) {
+    return null;
+  }
+  return (
+    <button onClick={handleCloseModal}>
+      <div>
+        {isOpen && (
+          <div className="fixed inset-0 z-50 ">
+            <div
+              id="sidebarContainer"
+              className="absolute inset-0 bg-black opacity-50"
+            ></div>
+            <div className="bg-white p-4 max-w-md z-10 h-screen w-64 relative">
+              <button className="absolute right-0" onClick={onClose}>
+                Close
+              </button>
+              {children}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </button>
   );
 };
 
