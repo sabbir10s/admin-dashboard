7e48e6e62eb8e7df75945cfb818ba0b776a2e108
sabbir10s <sabbirahmed1023@gmail.com>
2023-06-27 21:50:31 +0600

    testimonial full new update added

--- src/components/ui/Testimonial/TestimonialOne.jsx
+++ src/components/ui/Testimonial/TestimonialOne.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
 
+import StarRating from '../../common/StarRating/StarRating';
+
 const TestimonialOne = ({ testimonialInfo }) => {
-  const { title, name, occupation, company, testimonial } = testimonialInfo;
+  const { image, name, testimonial, occupation, rating } = testimonialInfo;
   return (
     <div>
-      <div>
-        <div className="flex flex-col rounded-xl  text-center shadow-md border-t-4 border-primary-300 shadow-gray-200 dark:shadow-gray-200/50 dark:bg-gray-900">
-          <div className="flex flex-1 flex-col justify-between p-6 lg:py-7 lg:px-5">
-            <div className="flex-1">
-              <p className="border-primary-500 px-10 text-xl dark:text-gray-300 font-black">
-                {title}
-              </p>
-
-              <blockquote className="mt-8 flex-1">
-                <p className="leading-relaxed text-primary-600">
-                  {testimonial}
-                </p>
-              </blockquote>
-            </div>
-
-            <div className="-mx-5 mt-8 px-8 py-1">
-              <div className="dark text-gray-500">
-                <p className="text-base font-bold">{name}</p>
-                <p className="mt-0.5 text-sm">
-                  {occupation}, {company}
-                </p>
-              </div>
-            </div>
+      <div className="mb-6 lg:mb-0">
+        <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-gray-800">
+          <div className="relative overflow-hidden bg-cover bg-no-repeat h-52">
+            <img
+              src={image}
+              className="w-full rounded-t-lg h-full object-cover"
+              alt=""
+            />
+            <a href="#!">
+              <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed"></div>
+            </a>
+            <svg
+              className="absolute left-0 bottom-0 text-white dark:text-gray-800"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 320"
+            >
+              <path
+                fill="currentColor"
+                d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+              ></path>
+            </svg>
+          </div>
+          <div className="p-6 ">
+            <h5 className="mb-2 text-lg font-bold dark:text-white">{name}</h5>
+            <h6 className="mb-4 font-medium text-primary dark:text-primary-400">
+              {occupation}
+            </h6>
+            <ul className="mb-6 flex justify-center">
+              <StarRating rating={rating} starSize={5} />
+            </ul>
+            <p className="text-gray-600 dark:text-gray-400">
+              {testimonial.slice(0, 100)}
+            </p>
           </div>
         </div>
       </div>
--- src/pages/Testimonials/Testimonials.jsx
+++ src/pages/Testimonials/Testimonials.jsx
@@ -5,7 +5,10 @@ const testimonials = [
   {
     _id: 1,
     title: 'Absolutely recommended!',
+    rating: 5,
     name: 'John Doe',
+    image:
+      'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/35af6a41332353.57a1ce913e889.jpg',
     occupation: 'CEO',
     company: 'ABC Corporation',
     testimonial:
@@ -14,6 +17,8 @@ const testimonials = [
   {
     _id: 2,
     title: 'Absolutely recommended!',
+    rating: 4,
+    image: 'https://mdbcdn.b-cdn.net/img/new/avatars/8.jpg',
     name: 'Jane Smith',
     occupation: 'Marketing Manager',
     company: 'XYZ Inc.',
@@ -23,7 +28,10 @@ const testimonials = [
   {
     _id: 3,
     title: 'Absolutely recommended!',
+    rating: 5,
     name: 'Michael Johnson',
+    image:
+      'https://images.unsplash.com/photo-1485206412256-701ccc5b93ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=594&q=80',
     occupation: 'Freelancer',
     company: 'XYZ',
     testimonial:
@@ -32,13 +40,18 @@ const testimonials = [
 ];
 const Testimonials = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
-      {testimonials.map((testimonialInfo) => (
-        <TestimonialOne
-          key={testimonialInfo._id}
-          testimonialInfo={testimonialInfo}
-        />
-      ))}
+    <div>
+      <h1 className="text-3xl font-semibold pb-10 dark:text-gray-200">
+        Testimonial
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
+        {testimonials.map((testimonialInfo) => (
+          <TestimonialOne
+            key={testimonialInfo._id}
+            testimonialInfo={testimonialInfo}
+          />
+        ))}
+      </div>
     </div>
   );
 };
